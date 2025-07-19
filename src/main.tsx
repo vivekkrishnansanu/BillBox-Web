a5556622cf409ce68f7432fc17ac897250d855a6
Vivek <vivekkrishnansanu@gmail.com>
2025-07-19 19:01:46 +0530

    Remove reset password logic and ensure all user data is stored/fetched from Supabase for cross-device sync

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ResetPassword from './components/ResetPassword';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
